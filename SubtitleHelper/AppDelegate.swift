22dd419d5971f364b5d8e8ab603350a7c7b0de66
Scott Robertson <spr@scottr.org>
2016-05-12 20:25:37 -0700

    Initial Commit

---
+++ SubtitleHelper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SubtitleHelper
+//
+//  Created by Scott Robertson on 5/12/16.
+//  Copyright © 2016 Scott Paul Robertson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SubtitleHelper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SubtitleHelper
+//
+//  Created by Scott Robertson on 5/12/16.
+//  Copyright © 2016 Scott Paul Robertson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
